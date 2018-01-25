325855488bd971f6f35d2590ca864ab64447027f
Daniel Huppmann <dh@dergelbesalon.at>
2018-01-25 12:58:05 +0100

    add unit tests for tutorials (#5)

---
+++ tests/test_tutorials.py
@@ -0,0 +1,69 @@
+import io
+import os
+import subprocess
+import sys
+import tempfile
+import nbformat
+import pytest
+
+from testing_utils import here
+
+from test_r import r_installed
+
+xport_path = os.path.join(here, '..', 'tutorial', 'transport')
+
+# taken from the execellent example here:
+# https://blog.thedataincubator.com/2016/06/testing-jupyter-notebooks/
+
+
+def _notebook_run(path, kernel=None):
+    """Execute a notebook via nbconvert and collect output.
+    :returns (parsed nb object, execution errors)
+    """
+    major_version = sys.version_info[0]
+    kernel = kernel or 'python{}'.format(major_version)
+    dirname, __ = os.path.split(path)
+    os.chdir(dirname)
+    with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
+        args = [
+            "jupyter", "nbconvert", "--to", "notebook", "--execute",
+            "--ExecutePreprocessor.timeout=60",
+            "--ExecutePreprocessor.kernel_name={}".format(kernel),
+            "--output", fout.name, path]
+        subprocess.check_call(args)
+
+        nb = nbformat.read(io.open(fout.name, encoding='utf-8'),
+                           nbformat.current_nbformat)
+
+    errors = [
+        output for cell in nb.cells if "outputs" in cell
+        for output in cell["outputs"] if output.output_type == "error"
+    ]
+
+    return nb, errors
+
+
+def test_py_transport():
+    fname = os.path.join(xport_path, 'py_transport.ipynb')
+    nb, errors = _notebook_run(fname)
+    assert errors == []
+
+
+def test_py_transport_scenario():
+    fname = os.path.join(xport_path, 'py_transport_scenario.ipynb')
+    nb, errors = _notebook_run(fname)
+    assert errors == []
+
+
+@pytest.mark.skipif(not r_installed(), reason='requires R to be installed')
+def test_R_transport():
+    fname = os.path.join(xport_path, 'R_transport.ipynb')
+    nb, errors = _notebook_run(fname, kernel='IR')
+    assert errors == []
+
+
+@pytest.mark.skipif(not r_installed(), reason='requires R to be installed')
+def test_R_transport_scenario():
+    fname = os.path.join(xport_path, 'R_transport_scenario.ipynb')
+    nb, errors = _notebook_run(fname, kernel='IR')
+    assert errors == []

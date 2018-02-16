ddc92712af2c24c31de6cb48afd72bccc32ef506
adrivinca <adrivinca@yahoo.it>
2018-02-16 10:03:12 +0100

    remove install_rixmp.bat

--- install_rixmp.bat
+++
@@ -1,9 +0,0 @@
-where /q r
-IF ERRORLEVEL 1 (
-    ECHO Error: R is missing. Ensure it is installed and placed in your PATH to build the r package.
-) ELSE (
-    rscript rixmp/build_rixmp.R [--verbose]
-)
-
-pause
-exit

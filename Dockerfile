9b60af2eac243c6b843e41bd161a520d0d67dede
Matthew Gidden <matthew.gidden@gmail.com>
2018-07-05 12:06:10 +0200

    update pdwrite to support multiple dfs for excel (#64)
    * update pdwrite to support multiple dfs for excel
    
    * add tests for pd io
    
    * add release note
    
    * appease stickler
    
    * appease stickler
    
    * consolidate is_pd
    
    * fix bugs
    
    * add xlswriter to container
    
    * add xlrd to container
    
    * add xl to other cis
    
    * correct names..

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,7 @@ COPY . /ixmp
 WORKDIR /
 ENV IXMP_PATH /ixmp
 ENV IXMP_R_PATH /ixmp/ixmp
+RUN pip2 install xlsxwriter xlrd
+RUN pip3 install xlsxwriter xlrd
 RUN cd /ixmp && python2 setup.py install 
 RUN cd /ixmp && python3 setup.py install
---
+++ tests/test_utils.py
@@ -0,0 +1,39 @@
+import os
+
+import pandas as pd
+import pandas.util.testing as pdt
+
+from ixmp import utils
+
+
+def make_obs(fname, exp, **kwargs):
+    utils.pd_write(exp, fname, index=False)
+    obs = utils.pd_read(fname, **kwargs)
+    os.remove(fname)
+    return obs
+
+
+def test_pd_io_csv():
+    fname = 'test.csv'
+    exp = pd.DataFrame({'a': [0, 1], 'b': [2, 3]})
+    obs = make_obs(fname, exp)
+    pdt.assert_frame_equal(obs, exp)
+
+
+def test_pd_io_xlsx():
+    fname = 'test.xlsx'
+    exp = pd.DataFrame({'a': [0, 1], 'b': [2, 3]})
+    obs = make_obs(fname, exp)
+    pdt.assert_frame_equal(obs, exp)
+
+
+def test_pd_io_xlsx_multi():
+    fname = 'test.xlsx'
+    exp = {
+        'sheet1': pd.DataFrame({'a': [0, 1], 'b': [2, 3]}),
+        'sheet2': pd.DataFrame({'c': [4, 5], 'd': [6, 7]}),
+    }
+    obs = make_obs(fname, exp, sheet_name=None)
+    for k, _exp in exp.items():
+        _obs = obs[k]
+        pdt.assert_frame_equal(_obs, _exp)

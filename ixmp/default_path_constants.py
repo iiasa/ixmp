382004c128ec0c02db3d9ef67b5bdc4a08d1f8ac
adrivinca <adrivinca@yahoo.it>
2018-07-10 14:37:57 +0200

    move up comment

--- ixmp/default_path_constants.py
+++ ixmp/default_path_constants.py
@@ -2,7 +2,8 @@ import os
 
 # path to local files
 LOCAL_PATH = os.path.expanduser(os.path.join('~', '.local', 'ixmp'))
-LOCAL_PATH = LOCAL_PATH.replace('Documents' + os.path.sep, '') # fix for R users
+# fix for R users
+LOCAL_PATH = LOCAL_PATH.replace('Documents' + os.path.sep, '') 
 
 # path to local configuration values
 CONFIG_PATH = os.path.join(LOCAL_PATH, 'config.json')

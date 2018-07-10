81d1f773d52479c5b94f719d527a293c9a7929ce
adrivinca <adrivinca@yahoo.it>
2018-07-10 14:05:01 +0200

    make path change for R users universal for any OS

--- ixmp/default_path_constants.py
+++ ixmp/default_path_constants.py
@@ -1,8 +1,8 @@
 import os
 
 # path to local files
-LOCAL_PATH = os.path.expanduser(os.path.join('~', '.local', 'ixmp')
-                                ).replace('Documents\\', '')
+LOCAL_PATH = os.path.expanduser(os.path.join('~', '.local', 'ixmp'))
+LOCAL_PATH = LOCAL_PATH.replace('Documents' + os.path.sep, '') # fix for R users
 
 # path to local configuration values
 CONFIG_PATH = os.path.join(LOCAL_PATH, 'config.json')

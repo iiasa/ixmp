19c749d492fdafe22ff80bc55377f056e5fb7c87
Nikolay Kushin <zikolach@gmail.com>
2018-11-13 14:53:16 +0100

    Update jarfile to current ixmp_source master (#94)
    - replace jarfile (compiled with jdk8)
    - fix migrations in test DB (ixmptest.script)
    - add start HSWLDB client script

---
+++ tests/testdb/start_client.sh
@@ -0,0 +1,5 @@
+java -cp ../../ixmp/lib/hsqldb.jar org.hsqldb.util.DatabaseManagerSwing \
+    --driver org.hsqldb.jdbcDriver \
+    --url jdbc:hsqldb:hsql://localhost:9001/ixmptest \
+    --user ixmp \
+    --password ixmp

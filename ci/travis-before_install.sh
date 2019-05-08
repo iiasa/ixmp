b6b8df25b11e2cb80baa62697c6bd6c3fb01c9c0
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-05-08 11:45:29 +0200

    Fiddle with Travis config 2

--- ci/travis-before_install.sh
+++ ci/travis-before_install.sh
@@ -1,35 +1,14 @@
-# Create cache directory
-CACHE=$HOME/.cache/ixmp
+set -x
+
+# Create required directories
 mkdir -p $CACHE
+mkdir -p $R_LIBS_USER
 
 # Download files into the cache directory
 maybe_download () {
-  echo "Starting download from $1"
   curl $1 --output $CACHE/$2 --remote-time --time-cond $CACHE/$2
-  echo "Download complete from $1"
+  chmod +x $CACHE/$2
 }
 
-# install gams
 maybe_download $GAMSURL $GAMSFNAME
-
-chmod +x $CACHE/$GAMSFNAME
-$CACHE/$GAMSFNAME > install.out
-which gams
-
-# install and update conda
 maybe_download $CONDAURL $CONDAFNAME
-
-chmod +x $CACHE/$CONDAFNAME
-$CACHE/$CONDAFNAME -b -p $HOME/miniconda
-conda update --yes conda
-
-# create named env
-conda create -n testing python=$PYVERSION --yes
-
-# install deps
-conda install -n testing -c conda-forge --yes \
-      ixmp \
-      pytest \
-      coveralls \
-      pytest-cov
-conda remove -n testing --force --yes ixmp
--- ci/travis-install.sh
+++ ci/travis-install.sh
@@ -1,5 +1,30 @@
+# Install GAMS
+$CACHE/$GAMSFNAME > install.out
+
+# Show location
+which gams
+
+
+# Install and update conda
+$CACHE/$CONDAFNAME -b -p $HOME/miniconda
+conda update --yes conda
+
+# Create named env
+conda create -n testing python=$PYVERSION --yes
+
+# Install deps
+conda install -n testing -c conda-forge --yes \
+      ixmp \
+      pytest \
+      coveralls \
+      pytest-cov
+conda remove -n testing --force --yes ixmp
+
+# Show information
+conda info --all
+
+
 # Install R packages needed for testing
-mkdir -p $R_LIBS_USER
 echo 'options(repos=c("https://cloud.r-project.org"))' >$R_PROFILE
 Rscript -e 'install.packages("devtools")'
 Rscript -e 'devtools::install_dev_deps("retixmp/source")'

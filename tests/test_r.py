c551c83395a5b15cb17760dab3e915a54e3cdebb
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-05-09 11:01:48 +0200

    Make tests_r backwards compatible

--- tests/test_r.py
+++ tests/test_r.py
@@ -33,8 +33,13 @@ def r_args(request, tmp_env, test_data_path):
     tmp_env['RETICULATE_PYTHON'] = sys.executable
     tmp_env['IXMP_TEST_DATA_PATH'] = str(test_data_path)
 
-    yield dict(cwd=retixmp_path, env=tmp_env, stdout=subprocess.PIPE,
-               stderr=subprocess.STDOUT, text=True)
+    args = dict(cwd=retixmp_path, env=tmp_env, stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT)
+
+    arg = 'text' if sys.version_info[:2] >= (3, 7) else 'universal_newlines'
+    args[arg] = True
+
+    yield args
 
 
 def test_r_build_and_check(r_args):
@@ -55,10 +60,21 @@ def test_r_build_and_check(r_args):
 
 def test_r_testthat(r_args):
     """Tests succeed on R code without building the package."""
+    # Python 2.7 compatibility
+    try:
+        from subprocess import run
+    except ImportError:
+        def run(*args, **kwargs):
+            popen = subprocess.Popen(*args, **kwargs)
+            popen.wait()
+            # Convert stream to sequence
+            popen.stdout = str(popen.stdout)
+            return popen
+
     tests_path = Path('tests', 'testthat')
     cmd = ['R', '--quiet', '-e', 'testthat::test_dir("{}")'.format(tests_path)]
 
-    info = subprocess.run(cmd, **r_args)
+    info = run(cmd, **r_args)
 
     # Number of testthat tests that failed
     failures = int(re.findall(r'Failed:\s*(\d*)', info.stdout)[0])

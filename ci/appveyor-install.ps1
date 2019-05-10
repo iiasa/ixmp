a78afc67d1d9cc52266c13fc9c9a497ba896d6a7
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-05-10 11:09:15 +0200

    Fiddle with Appveyor config 20

--- ci/appveyor-install.ps1
+++ ci/appveyor-install.ps1
@@ -8,19 +8,24 @@ Start-FileDownload 'https://d37drm4t2jghv5.cloudfront.net/distributions/25.1.1/w
 & ".\windows_x64_64.exe" "/SP- /NORESTART /DIR=.\gams /NOICONS"
 
 $env:PATH = $(Get-Location).Path + '\gams;' + $env:PATH
-Write-Output $env:PATH
 
 # Update conda
 
 # These correspond to folder naming of miniconda installs on appveyor
 # See https://www.appveyor.com/docs/windows-images-software/#miniconda
-$MC_PYTHON_VERSION = $env:PYTHON_VERSION.Replace('.', '')
+if ( $env:PYTHON_VERSION -eq '2.7' ) {
+  $MC_PYTHON_VERSION = ''
+} else {
+  $MC_PYTHON_VERSION = $env:PYTHON_VERSION.Replace('.', '')
+}
 if ( $env:PYTHON_ARCH -eq '64' ) { $ARCH_LABEL = '-x64' }
 
 $CR = 'C:\Miniconda' + $MC_PYTHON_VERSION + $ARCH_LABEL
 
 $env:CONDA_ROOT = $CR
 $env:PATH = $CR + ';' + $CR + '\Scripts;' + $CR + '\Library\bin;' + $env:PATH
+
+Write-Output $env:PATH
 
 conda update --yes conda
 

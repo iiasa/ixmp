7da33ebd353bf1ba284069196b92a424652fd2c9
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-05-11 10:51:06 +0200

    Do not build manual on Appveyor

--- tests/test_r.py
+++ tests/test_r.py
@@ -67,8 +67,11 @@ def test_r_build_and_check(r_args):
     subprocess.check_call(cmd, **r_args)
 
     # Path() here is required because of str() in r_args for Python 2.7 compat
-    # --no-multiarch is for Windows/Appveyor
-    cmd = ['R', 'CMD', 'check', '--no-multiarch']
+    cmd = ['R', 'CMD', 'check']
+    if 'APPVEYOR' in r_args['env']:
+        # - Do not cross-build/-check e.g. i386 on x64 Appveyor workers
+        # - Do not build manual (avoids overhead of LaTeX install)
+        cmd.extend(['--no-multiarch', '--no-manual'])
     cmd.extend(map(str, Path(r_args['cwd']).glob('*.tar.gz')))
     info = run(cmd, **r_args)
 

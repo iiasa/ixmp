93221294414c7c689bbc9ab50419f7fa447c23c5
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-05-11 13:32:05 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,3 +1,8 @@
+rem Install ixmp and retixmp from source on Windows
+rem
+rem TODO if this script is to be kept/supported, describe it in the docs.
+
+rem TODO remove?
 cd > .foo
 set /p IXMP=<.foo
 del .foo
@@ -18,12 +23,18 @@ where /q r
 IF ERRORLEVEL 1 (
     ECHO No valid installation of R found, skipped build and installation of R package.
 ) ELSE (
-    rscript rixmp/build_rixmp.R [--verbose]
-    rscript retixmp/build_retixmp.R [--verbose]
+    rem TODO remove?
+    rem rscript rixmp/build_rixmp.R [--verbose]
+
+    cd retixmp/source
+    R CMD build .
+    R CMD INSTALL --html retixmp_0.2.0.9000.tar.gz
 )
 
+rem TODO remove?
 setx IXMP_PATH "%IXMP%"
 setx IXMP_R_PATH "%RPTH%/ixmp"
+
 py.test tests/
 
 pause

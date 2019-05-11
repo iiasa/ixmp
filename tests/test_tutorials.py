d4594bfca9677c8629d9797bfada591b4111963f
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-05-11 13:13:36 +0200

    Remove pytest config for skipping Windows tests

--- tests/test_tutorials.py
+++ tests/test_tutorials.py
@@ -5,7 +5,6 @@ from ixmp.testing import get_cell_output, run_notebook
 from test_r import r_installed
 
 
-#@pytest.mark.skip_win_ci
 def test_py_transport(tutorial_path, tmp_path, tmp_env):
     fname = tutorial_path / 'transport' / 'py_transport.ipynb'
     nb, errors = run_notebook(fname, tmp_path, tmp_env)
@@ -15,7 +14,6 @@ def test_py_transport(tutorial_path, tmp_path, tmp_env):
     assert np.isclose(get_cell_output(nb, -5)['lvl'], 153.6750030517578)
 
 
-#@pytest.mark.skip_win_ci
 def test_py_transport_scenario(tutorial_path, tmp_path, tmp_env):
     fname = tutorial_path / 'transport' / 'py_transport_scenario.ipynb'
     nb, errors = run_notebook(fname, tmp_path, tmp_env)

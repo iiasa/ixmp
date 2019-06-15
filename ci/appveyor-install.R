8b2b988256e81de9f717f487dc9103888bbadb93
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-06-15 19:03:17 +0200

    Fix Appveyor error with R mime 0.7 2

--- ci/appveyor-install.R
+++ ci/appveyor-install.R
@@ -6,7 +6,7 @@ if ( step == '1' ) {
   install.packages(c('devtools', 'IRkernel'), quiet = TRUE)
 
   # mime 0.7 (2019-06-11) not yet available as binary; attempt to install from
-  # source causes compilation error
+  # source causes a compilation error on Appveyor
   url <- 'https://cran.r-project.org/bin/windows/contrib/3.6/mime_0.6.zip'
   install.packages(url, repos = NULL)
 
@@ -14,7 +14,10 @@ if ( step == '1' ) {
 
 } else if ( step == '2' ) {
 
+  # upgrade = FALSE is to keep mime (above) at 0.6
   devtools::install(file.path('.', 'rixmp'),
-                    args = '--no-multiarch', dependencies = TRUE)
+                    args = '--no-multiarch',
+                    dependencies = TRUE,
+                    upgrade = FALSE)
 
 }

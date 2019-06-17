39effaffcc1e2767b27aaa7ebe16cf6849215202
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-06-17 11:35:29 +0200

    Purge unused code in install.bat

--- install.bat
+++ install.bat
@@ -2,17 +2,6 @@ rem Install ixmp and rixmp from source on Windows
 rem
 rem TODO if this script is to be kept/supported, describe it in the docs.
 
-rem TODO remove?
-rem cd > .foo
-rem set /p IXMP=<.foo
-rem del .foo
-rem echo %IXMP%
-rem
-rem python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())" > .foo
-rem set /p RPTH=<.foo
-rem del .foo
-rem echo %RPTH%
-
 @echo off
 
 python setup.py install
@@ -23,22 +12,14 @@ where /q r
 IF ERRORLEVEL 1 (
   echo R not installed; skipping installation of rixmp.
 ) ELSE (
-    rem TODO remove?
-    rem rscript rixmp.legacy/build_rixmp.legacy.R [--verbose]
-
-    cd rixmp
-    R CMD build .
-    R CMD INSTALL --html rixmp_0.1.3.9000.tar.gz
-    cd ..
+  cd rixmp
+  R CMD build .
+  R CMD INSTALL --html rixmp_0.1.3.9000.tar.gz
+  cd ..
 )
-
-rem TODO remove?
-rem setx IXMP_PATH "%IXMP%"
-rem setx IXMP_R_PATH "%RPTH%/ixmp"
 
 pytest
 
-pause
 exit
 
 @rem install error

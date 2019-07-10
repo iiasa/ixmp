45c2d089837b58f4c3cab436867e13bccf77f8ae
Nikolay Kushin <zikolach@gmail.com>
2019-07-10 16:34:57 +0200

    Removing as suggested by @khaeru

--- ci/appveyor-install.R
+++ ci/appveyor-install.R
@@ -5,11 +5,6 @@ options(repos=c('https://cloud.r-project.org'))
 if ( step == '1' ) {
   install.packages(c('devtools', 'IRkernel'), quiet = TRUE)
 
-  # mime 0.7 (2019-06-11) not yet available as binary; attempt to install from
-  # source causes a compilation error on Appveyor
-  url <- 'https://cran.r-project.org/bin/windows/contrib/3.6/mime_0.6.zip'
-  install.packages(url, repos = NULL)
-
   IRkernel::installspec()
 
 } else if ( step == '2' ) {

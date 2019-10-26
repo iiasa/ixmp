b2fea0525b2c03d0c62c0c6fdd1cc2cd951d1b6c
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-10-26 15:08:49 +0200

    Update ixmp._version.get_config to match setup.cfg

--- ixmp/_version.py
+++ ixmp/_version.py
@@ -40,7 +40,7 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440"
+    cfg.style = "pep440-pre"
     cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "ixmp-"
     cfg.versionfile_source = "ixmp/_version.py"

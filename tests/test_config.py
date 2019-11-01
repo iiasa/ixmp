767b3e837b51cc5f4035055625202345af117b57
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-11-01 18:41:49 +0100

    Remove obsolete test_config.test_get

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,18 +1,6 @@
-try:
-    from pathlib import Path
-except ImportError:
-    from pathlib2 import Path
-
 import pytest
 
 from ixmp._config import Config
-
-
-try:
-    FileNotFoundError
-except NameError:
-    # Python 2.7
-    FileNotFoundError = OSError
 
 
 @pytest.fixture
@@ -24,11 +12,6 @@ def cfg():
 def test_locate_nonexistent(cfg):
     with pytest.raises(FileNotFoundError):
         cfg._locate('nonexistent')
-
-
-def test_get(cfg):
-    # This key has a value even with no configuration provided
-    assert cfg.get('DEFAULT_LOCAL_DB_PATH')
 
 
 def test_config_default(cfg):

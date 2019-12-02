657aa5181f9ce61676c407e45cdcec5a16f19e9c
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-12-02 09:15:29 +0100

    Adjust test_cache_non_hashable for AppVeyor/Python 3.6

--- tests/backend/test_base.py
+++ tests/backend/test_base.py
@@ -7,6 +7,7 @@ def test_cache_non_hashable():
     filters = {'s': ['foo', 42, object()]}
 
     # _cache_key() cannot handle non-hashable object()
-    with pytest.raises(TypeError,
-                       match='Object of type object is not JSON serializable'):
+    # NB exception message contains single quotes on 'object' on Windows/py3.6
+    with pytest.raises(TypeError, match="Object of type .?object.? is not JSON"
+                                        " serializable"):
         CachingBackend._cache_key(object(), 'par', 'p', filters)

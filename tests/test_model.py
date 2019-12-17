a4b98aaa6d0afb9dc99408cc776fc5387e2b8074
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-12-17 13:43:14 +0100

    Model.initialize() works on already-initialized Scenario

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,5 +1,10 @@
-from ixmp.testing import make_dantzig
+import logging
+
 import pytest
+
+from ixmp import Scenario
+from ixmp.testing import make_dantzig
+from ixmp.model.dantzig import DantzigModel
 
 
 @pytest.mark.parametrize('kwargs', [
@@ -12,11 +17,26 @@ def test_GAMSModel(test_mp, test_data_path, kwargs):
     s.solve(model='dantzig', **kwargs)
 
 
-def test_model_initialize():
-    # TODO Model.initialize() runs on both a 'empty' and already-init'd
-    #      Scenario
+def test_model_initialize(test_mp, caplog):
+    # Model.initialize runs on an empty Scenario
+    s = make_dantzig(test_mp)
+    b = s.par('b')
+    assert len(b) == 3
 
-    # TODO Unrecognized Scenario(scheme=...) raises an intelligible exception
+    # TODO modify a value for 'b' and ensure it is not overwritten when
+    #      initialize is called again.
+
+    # Model.initialize runs on an already initialized Scenario
+    DantzigModel.initialize(s, with_data=True)
+    assert len(s.par('b')) == 3
+
+    # Unrecognized Scenario(scheme=...) is initialized using the base method, a
+    # no-op
+    caplog.set_level(logging.DEBUG)
+    Scenario(test_mp, model='model name', scenario='scenario name',
+             version='new', scheme='unknown')
+    assert caplog.records[-1].message == \
+        "No initialization for 'unknown'-scheme Scenario"
 
     # TODO Keyword arguments to Scenario(...) that are not recognized by
     #      Model.initialize() raise an intelligible exception

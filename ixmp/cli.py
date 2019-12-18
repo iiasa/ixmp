21f48280114c3cad0afe2b769b946bc2e620fa53
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-12-18 23:03:47 +0100

    Prototype of 'ixmp list' CLI command

--- ixmp/cli.py
+++ ixmp/cli.py
@@ -112,3 +112,49 @@ def platform(action, name, values):
     elif action == 'list':
         for key, info in ixmp.config.values['platform'].items():
             print(key, info)
+
+
+@main.command('list')
+@click.option('--default-only', is_flag=True,
+              help='Only display scenarios with a default version.')
+@click.pass_obj
+def list_command(context, default_only):
+    """List scenarios on the --platform."""
+    scenarios = context['mp'].scenario_list(default=default_only)
+
+    N_model = 0
+
+    for model, group_m in scenarios.groupby('model'):
+        N_model += 1
+        print('\n', model, sep='')
+
+        for scenario_name, group_s in group_m.groupby('scenario'):
+            max = group_s.version.max()
+
+            try:
+                mask = group_s.is_default.astype(bool)
+                default = group_s.loc[mask, 'version'].iat[0]
+            except IndexError:
+                default = max
+
+            line = '  {}#{}'.format(scenario_name, default)
+
+            count = len(group_s)
+            if count > 1:
+                line += '\t'
+                min = group_s.version.min()
+
+                if default != max:
+                    line += '{}–{}'.format(min, max)
+
+                line += '({} versions)'.format(count)
+
+            print(line)
+
+    print(
+        '',
+        '{} model name(s)'.format(N_model),
+        '{} scenario name(s)'.format(len(scenarios['scenario'].unique())),
+        '{} (model, scenario) combination(s)'
+        .format(len(scenarios.groupby(['model', 'scenario']))),
+        sep='\n')

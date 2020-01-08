864dc317a719d2310ce290b10439b3a53c96524f
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-01-08 16:44:59 +0100

    Debug Java exceptions on Windows 2

--- ixmp/backend/jdbc.py
+++ ixmp/backend/jdbc.py
@@ -759,7 +759,6 @@ def start_jvm(jvmargs=None):
     if os.name == 'nt':
         print('jpype.getDefaultJVMPath():', args)
         print('JAVA_HOME:', os.environ.get('JAVA_HOME', '(not set)'))
-        assert False
 
     # Add the ixmp root directory, ixmp.jar and bundled .jar and .dll files to
     # the classpath

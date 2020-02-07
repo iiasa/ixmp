74cb846c4c3cb309b5d1618158ce6934d7f808e3
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-02-07 13:03:25 +0100

    Adjust tutorial py_transport_scenario to remove deprecated calls

--- tests/test_tutorials.py
+++ tests/test_tutorials.py
@@ -18,9 +18,8 @@ def test_py_transport_scenario(tutorial_path, tmp_path, tmp_env):
     nb, errors = run_notebook(fname, tmp_path, tmp_env)
     assert errors == []
 
-    # FIXME use get_cell_by_name instead of assuming cell count/order is fixed
-    assert np.isclose(get_cell_output(nb, -9)['lvl'], 153.6750030517578)
-    assert np.isclose(get_cell_output(nb, -8)['lvl'], 161.3249969482422)
+    assert np.isclose(get_cell_output(nb, 'scen-z')['lvl'], 153.675)
+    assert np.isclose(get_cell_output(nb, 'scen-detroit-z')['lvl'], 161.324)
 
 
 @pytest.mark.rixmp

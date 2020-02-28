910379ec101f8ae97d7f9f222f3bd20290abcbb2
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-02-28 12:56:37 +0100

    Simplify handling of expected data in TimeSeries tests

--- tests/test_feature_timeseries.py
+++ tests/test_feature_timeseries.py
@@ -17,23 +17,24 @@ cols_str = ['region', 'variable', 'unit', 'year']
 # NB the columns are in a specific order; model and scenario come last in the
 #    data returned by ixmp.
 # TODO fix this; model and scenario should come first, matching the IAMC order.
-TS_DF = pd.DataFrame.from_dict(dict(
-    region='World',
-    variable='Testing',
-    unit='???',
-    year=[2010, 2020],
-    value=[23.7, 23.8],
-    **test_args
-))
-
-TS_2050 = pd.DataFrame.from_dict(dict(
-    region='World',
-    variable='Testing',
-    unit='???',
-    year=[2000, 2010, 2020, 2030, 2040, 2050],
-    value=[21.7, 22.7, 23.7, 24.7, 25.7, 25.8],
-    **test_args
-))
+DATA = {
+    0: pd.DataFrame.from_dict(dict(
+        region='World',
+        variable='Testing',
+        unit='???',
+        year=[2010, 2020],
+        value=[23.7, 23.8],
+        **test_args
+    )),
+    2050: pd.DataFrame.from_dict(dict(
+        region='World',
+        variable='Testing',
+        unit='???',
+        year=[2000, 2010, 2020, 2030, 2040, 2050],
+        value=[21.7, 22.7, 23.7, 24.7, 25.7, 25.8],
+        **test_args
+    )),
+}
 
 
 # Fixtures
@@ -63,44 +64,44 @@ def expected(df, ts):
     return df.assign(model=ts.model, scenario=ts.scenario)
 
 
+def wide(df):
+    return df.pivot_table(index=IAMC_IDX, columns='year', values='value') \
+             .reset_index()
+
+
 def test_get_timeseries(ts):
-    assert_frame_equal(TS_DF, ts.timeseries())
+    assert_frame_equal(DATA[0], ts.timeseries())
 
 
 def test_get_timeseries_iamc(ts):
-    obs = ts.timeseries(region='World', variable='Testing', iamc=True)
-
-    exp = TS_DF.pivot_table(index=IAMC_IDX, columns='year', values='value') \
-        .reset_index() \
-        .rename_axis(columns=None)
-
-    assert_frame_equal(exp, obs)
+    exp = wide(DATA[0]).rename_axis(columns=None)
+    assert_frame_equal(exp, ts.timeseries(iamc=True))
 
 
 def test_new_timeseries_as_year_value(test_mp):
     ts = ixmp.TimeSeries(test_mp, **test_args, version='new')
-    ts.add_timeseries(TS_DF)
+    ts.add_timeseries(DATA[0])
     ts.commit('importing a testing timeseries')
-    assert_frame_equal(TS_DF, ts.timeseries())
+    assert_frame_equal(DATA[0], ts.timeseries())
 
 
 def test_new_timeseries_as_iamc(test_mp):
     ts = ixmp.TimeSeries(test_mp, **test_args, version='new')
-    ts.add_timeseries(TS_DF.pivot_table(values='value', index=cols_str))
+    ts.add_timeseries(wide(DATA[0]))
     ts.commit('importing a testing timeseries')
-    assert_frame_equal(TS_DF, ts.timeseries())
+    assert_frame_equal(DATA[0], ts.timeseries())
 
 
 def test_new_timeseries_error(ts_empty):
     # Error: column 'unit' is missing
     with pytest.raises(ValueError):
-        ts_empty.add_timeseries(TS_DF.drop('unit', axis=1))
+        ts_empty.add_timeseries(DATA[0].drop('unit', axis=1))
 
 
 def test_timeseries_edit(mp, ts):
     # Overwrite existing data
     ts.check_out(timeseries_only=True)
-    ts.add_timeseries(TS_DF)
+    ts.add_timeseries(DATA[0])
     ts.commit('testing of editing timeseries (same years)')
 
     # Overwrite and add new values at once
@@ -120,63 +121,60 @@ def test_timeseries_edit(mp, ts):
     mp.open_db()
 
     # All four rows are retrieved
-    exp = pd.concat([TS_DF.loc[0:0, :], df]).reset_index(drop=True)
+    exp = pd.concat([DATA[0].loc[0:0, :], df]).reset_index(drop=True)
     assert_frame_equal(exp, ts.timeseries())
 
 
 def test_timeseries_edit_iamc(mp, ts_empty):
     ts = ts_empty
     info = dict(model=ts.model, scenario=ts.scenario)
-    exp = expected(TS_DF, ts)
+    exp = expected(DATA[0], ts)
 
-    ts.add_timeseries(TS_DF)
+    ts.add_timeseries(DATA[0])
     ts.commit('updating timeseries in IAMC format')
 
     ts = ixmp.TimeSeries(mp, **info)
     assert_frame_equal(exp, ts.timeseries())
 
     ts.check_out(timeseries_only=True)
-    df = TS_2050.pivot_table(index=IAMC_IDX, columns='year', values='value') \
-                .reset_index()
+    df = wide(DATA[2050])
     ts.add_timeseries(df)
     ts.commit('updating timeseries in IAMC format')
 
-    exp = expected(TS_2050, ts)
+    exp = expected(DATA[2050], ts)
     assert_frame_equal(exp, ts.timeseries())
 
 
 def test_timeseries_edit_with_region_synonyms(mp, ts_empty):
     ts = ts_empty
     info = dict(model=ts.model, scenario=ts.scenario)
-    exp = expected(TS_DF, ts)
+    exp = expected(DATA[0], ts)
 
     mp.set_log_level('DEBUG')
     mp.add_region_synonym('Hell', 'World')
 
-    ts.add_timeseries(TS_DF)
+    ts.add_timeseries(DATA[0])
     ts.commit('updating timeseries in IAMC format')
 
     ts = ixmp.TimeSeries(mp, **info)
     assert_frame_equal(exp, ts.timeseries())
 
     ts.check_out(timeseries_only=True)
-    df = TS_2050.pivot_table(index=IAMC_IDX, columns='year', values='value') \
-                .reset_index() \
-                .replace('World', 'Hell')
+    df = wide(DATA[2050]).replace('World', 'Hell')
     ts.preload_timeseries()
     ts.add_timeseries(df)
     ts.commit('updating timeseries in IAMC format')
 
-    exp = expected(TS_2050, ts)
+    exp = expected(DATA[2050], ts)
     assert_frame_equal(exp, ts.timeseries())
 
 
 def test_timeseries_remove_single_entry(mp, ts_empty):
     ts = ts_empty
     info = dict(model=ts.model, scenario=ts.scenario)
-    exp = expected(TS_DF, ts)
+    exp = expected(DATA[0], ts)
 
-    ts.add_timeseries(TS_DF.pivot_table(values='value', index=cols_str))
+    ts.add_timeseries(wide(DATA[0]))
     ts.commit('importing a testing timeseries')
 
     ts = ixmp.TimeSeries(mp, **info)
@@ -193,9 +191,9 @@ def test_timeseries_remove_single_entry(mp, ts_empty):
 def test_timeseries_remove_all_data(mp, ts_empty):
     ts = ts_empty
     info = dict(model=ts.model, scenario=ts.scenario)
-    exp = expected(TS_DF, ts)
+    exp = expected(DATA[0], ts)
 
-    ts.add_timeseries(TS_DF.pivot_table(values='value', index=cols_str))
+    ts.add_timeseries(wide(DATA[0]))
     ts.commit('importing a testing timeseries')
 
     ts = ixmp.TimeSeries(mp, **info)
@@ -205,7 +203,7 @@ def test_timeseries_remove_all_data(mp, ts_empty):
 
     ts.check_out()
     ts.add_timeseries(exp)
-    ts.remove_timeseries(TS_DF)
+    ts.remove_timeseries(DATA[0])
     ts.commit('testing for removing a full timeseries row')
 
     assert ts.timeseries(region='World', variable='Testing').empty

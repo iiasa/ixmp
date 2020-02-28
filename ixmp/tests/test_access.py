2ab628617da8662d87eac5d63c3c073336369d42
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-02-28 21:38:38 +0100

    Increase test_access timeout to 1.0 seconds for Travis/macOS

--- ixmp/tests/test_access.py
+++ ixmp/tests/test_access.py
@@ -20,7 +20,7 @@ def mock():
     print('Mock server started with pid {}'.format(proc.pid))
 
     # Wait for server to start up
-    sleep(0.5)
+    sleep(1)
 
     yield HTTPMock('localhost', 8000)
 

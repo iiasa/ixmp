5b02c6bbad7bcb7158e1992c1865a14816ddeff6
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-02-28 11:21:28 +0100

    Refactor test_feature_timeseries.py using new fixtures

--- tests/test_feature_timeseries.py
+++ tests/test_feature_timeseries.py
@@ -1,129 +1,140 @@
 import pandas as pd
+from pandas.testing import assert_frame_equal, assert_series_equal
 import pytest
-from numpy import testing as npt
 
 import ixmp
+from ixmp.core import IAMC_IDX
+from ixmp.testing import populate_test_platform
+
 
 test_args = ('Douglas Adams', 'Hitchhiker')
 
 # string columns and dataframe for timeseries checks
-iamc_idx_cols = ['model', 'scenario', 'region', 'variable', 'unit']
 cols_str = ['region', 'variable', 'unit', 'year']
 
-TS_DF = {'year': [2010, 2020], 'value': [23.7, 23.8]}
-TS_DF = pd.DataFrame.from_dict(TS_DF)
-TS_DF['region'] = 'World'
-TS_DF['variable'] = 'Testing'
-TS_DF['unit'] = '???'
+TS_DF = pd.DataFrame.from_dict(dict(
+    region='World', variable='Testing', unit='???', year=[2010, 2020],
+    value=[23.7, 23.8]))
 
 
-def test_get_timeseries(test_mp):
-    scen = ixmp.TimeSeries(test_mp, *test_args)
-    assert_timeseries(scen)
+# Fixtures
+@pytest.fixture(scope='class')
+def mp(test_mp):
+    populate_test_platform(test_mp)
+    yield test_mp
 
 
-def test_get_timeseries_iamc(test_mp):
-    scen = ixmp.TimeSeries(test_mp, *test_args)
-    obs = scen.timeseries(region='World', variable='Testing', iamc=True)
+@pytest.fixture(scope='class')
+def ts(mp):
+    yield ixmp.TimeSeries(mp, model='Douglas Adams', scenario='Hitchhiker')
 
-    exp = TS_DF.pivot_table(index=['region', 'variable', 'unit'],
-                            columns='year')['value'].reset_index()
-    exp['model'] = 'Douglas Adams'
-    exp['scenario'] = 'Hitchhiker'
 
-    npt.assert_array_equal(exp[iamc_idx_cols], obs[iamc_idx_cols])
-    npt.assert_array_almost_equal(exp[2010], obs[2010])
+# Assertions
+def assert_timeseries(ts, exp=TS_DF):
+    obs = ts.timeseries(region='World')
+    assert_frame_equal(exp[cols_str], obs[cols_str])
+    assert_series_equal(exp['value'], obs['value'])
+
+
+def test_get_timeseries(ts):
+    assert_timeseries(ts)
+
+
+def test_get_timeseries_iamc(ts):
+    obs = ts.timeseries(region='World', variable='Testing', iamc=True)
+
+    exp = TS_DF.assign(model='Douglas Adams', scenario='Hitchhiker') \
+        .pivot_table(index=IAMC_IDX, columns='year')['value'] \
+        .reset_index() \
+        .rename_axis(columns=None)
+
+    assert_frame_equal(exp, obs)
 
 
 def test_new_timeseries_as_year_value(test_mp):
-    scen = ixmp.TimeSeries(test_mp, *test_args, version='new', annotation='fo')
-    scen.add_timeseries(TS_DF)
-    scen.commit('importing a testing timeseries')
-    assert_timeseries(scen)
+    ts = ixmp.TimeSeries(test_mp, *test_args, version='new', annotation='fo')
+    ts.add_timeseries(TS_DF)
+    ts.commit('importing a testing timeseries')
+    assert_timeseries(ts)
 
 
 def test_new_timeseries_as_iamc(test_mp):
-    scen = ixmp.TimeSeries(test_mp, *test_args, version='new', annotation='fo')
-    scen.add_timeseries(TS_DF.pivot_table(values='value', index=cols_str))
-    scen.commit('importing a testing timeseries')
-    assert_timeseries(scen)
-
-
-def assert_timeseries(scen, exp=TS_DF):
-    obs = scen.timeseries(region='World')
-    npt.assert_array_equal(exp[cols_str], obs[cols_str])
-    npt.assert_array_almost_equal(exp['value'], obs['value'])
+    ts = ixmp.TimeSeries(test_mp, *test_args, version='new', annotation='fo')
+    ts.add_timeseries(TS_DF.pivot_table(values='value', index=cols_str))
+    ts.commit('importing a testing timeseries')
+    assert_timeseries(ts)
 
 
 def test_new_timeseries_error(test_mp):
-    scen = ixmp.TimeSeries(test_mp, *test_args, version='new', annotation='fo')
+    ts = ixmp.TimeSeries(test_mp, *test_args, version='new', annotation='fo')
     df = {'year': [2010, 2020], 'value': [23.5, 23.6]}
     df = pd.DataFrame.from_dict(df)
     df['region'] = 'World'
     df['variable'] = 'Testing'
     # column `unit` is missing
-    pytest.raises(ValueError, scen.add_timeseries, df)
+    pytest.raises(ValueError, ts.add_timeseries, df)
 
 
-def test_timeseries_edit(test_mp):
-    scen = ixmp.TimeSeries(test_mp, *test_args)
+def test_timeseries_edit(mp, ts):
     df = {'region': ['World'] * 2, 'variable': ['Testing'] * 2,
           'unit': ['???', '???'], 'year': [2010, 2020], 'value': [23.7, 23.8]}
     exp = pd.DataFrame.from_dict(df)
-    obs = scen.timeseries()
-    npt.assert_array_equal(exp[cols_str], obs[cols_str])
-    npt.assert_array_almost_equal(exp['value'], obs['value'])
+    obs = ts.timeseries()
+    assert_frame_equal(exp[cols_str], obs[cols_str])
+    assert_series_equal(exp['value'], obs['value'])
 
-    scen.check_out(timeseries_only=True)
+    ts.check_out(timeseries_only=True)
     df = {'region': ['World'] * 2,
           'variable': ['Testing'] * 2,
           'unit': ['???', '???'], 'year': [2010, 2020],
           'value': [23.7, 23.8]}
     df = pd.DataFrame.from_dict(df)
-    scen.add_timeseries(df)
-    scen.commit('testing of editing timeseries (same years)')
+    ts.add_timeseries(df)
+    ts.commit('testing of editing timeseries (same years)')
 
-    scen.check_out(timeseries_only=True)
+    ts.check_out(timeseries_only=True)
     df = {'region': ['World'] * 3,
           'variable': ['Testing', 'Testing', 'Testing2'],
           'unit': ['???', '???', '???'], 'year': [2020, 2030, 2030],
           'value': [24.8, 24.9, 25.1]}
     df = pd.DataFrame.from_dict(df)
-    scen.add_timeseries(df)
-    scen.commit('testing of editing timeseries (other years)')
-    test_mp.close_db()
+    ts.add_timeseries(df)
+    ts.commit('testing of editing timeseries (other years)')
+    mp.close_db()
 
-    test_mp.open_db()
-    scen = ixmp.TimeSeries(test_mp, *test_args)
-    obs = scen.timeseries().sort_values(by=['year'])
-    df = df.append(exp.loc[0]).sort_values(by=['year'])
-    npt.assert_array_equal(df[cols_str], obs[cols_str])
-    npt.assert_array_almost_equal(df['value'], obs['value'])
+    mp.open_db()
+    ts = ixmp.TimeSeries(mp, *test_args)
+    obs = ts.timeseries().sort_values(by=['year'])
+    df = df.append(exp.loc[0]) \
+        .sort_values(by=['year']) \
+        .reset_index()
+    assert_frame_equal(df[cols_str], obs[cols_str])
+    assert_series_equal(df['value'], obs['value'])
 
 
 def test_timeseries_edit_iamc(test_mp):
     args_all = ('Douglas Adams 1', 'test_remove_all')
-    scen = ixmp.TimeSeries(test_mp, *args_all, version='new', annotation='nk')
+    ts = ixmp.TimeSeries(test_mp, *args_all, version='new', annotation='nk')
 
     df = pd.DataFrame.from_dict({'region': ['World'],
                                  'variable': ['Testing'],
                                  'unit': ['???'],
                                  '2010': [23.7],
                                  '2020': [23.8]})
-    scen.add_timeseries(df)
-    scen.commit('updating timeseries in IAMC format')
+    ts.add_timeseries(df)
+    ts.commit('updating timeseries in IAMC format')
 
-    scen = ixmp.TimeSeries(test_mp, *args_all)
-    obs = scen.timeseries()
+    ts = ixmp.TimeSeries(test_mp, *args_all)
+    obs = ts.timeseries()
     exp = pd.DataFrame.from_dict({'region': ['World', 'World'],
                                   'variable': ['Testing', 'Testing'],
                                   'unit': ['???', '???'],
                                   'year': [2010, 2020],
                                   'value': [23.7, 23.8]})
-    npt.assert_array_equal(exp[cols_str], obs[cols_str])
-    npt.assert_array_almost_equal(exp['value'], obs['value'])
+    assert_frame_equal(exp[cols_str], obs[cols_str])
+    assert_series_equal(exp['value'], obs['value'])
 
-    scen.check_out(timeseries_only=True)
+    ts.check_out(timeseries_only=True)
     df = pd.DataFrame.from_dict({'region': ['World'],
                                  'variable': ['Testing'],
                                  'unit': ['???'],
@@ -133,8 +144,8 @@ def test_timeseries_edit_iamc(test_mp):
                                  '2030': [24.7],
                                  '2040': [25.7],
                                  '2050': [25.8]})
-    scen.add_timeseries(df)
-    scen.commit('updating timeseries in IAMC format')
+    ts.add_timeseries(df)
+    ts.commit('updating timeseries in IAMC format')
 
     exp = pd.DataFrame.from_dict(
         {'region': ['World'] * 6,
@@ -142,9 +153,9 @@ def test_timeseries_edit_iamc(test_mp):
          'unit': ['???'] * 6,
          'year': [2000, 2010, 2020, 2030, 2040, 2050],
          'value': [21.7, 22.7, 23.7, 24.7, 25.7, 25.8]})
-    obs = scen.timeseries()
-    npt.assert_array_equal(exp[cols_str], obs[cols_str])
-    npt.assert_array_almost_equal(exp['value'], obs['value'])
+    obs = ts.timeseries()
+    assert_frame_equal(exp[cols_str], obs[cols_str])
+    assert_series_equal(exp['value'], obs['value'])
     test_mp.close_db()
 
 
@@ -152,27 +163,27 @@ def test_timeseries_edit_with_region_synonyms(test_mp):
     args_all = ('Douglas Adams 1', 'test_remove_all')
     test_mp.set_log_level('DEBUG')
     test_mp.add_region_synonym('Hell', 'World')
-    scen = ixmp.TimeSeries(test_mp, *args_all, version='new', annotation='nk')
+    ts = ixmp.TimeSeries(test_mp, *args_all, version='new', annotation='nk')
 
     df = pd.DataFrame.from_dict({'region': ['World'],
                                  'variable': ['Testing'],
                                  'unit': ['???'],
                                  '2010': [23.7],
                                  '2020': [23.8]})
-    scen.add_timeseries(df)
-    scen.commit('updating timeseries in IAMC format')
+    ts.add_timeseries(df)
+    ts.commit('updating timeseries in IAMC format')
 
-    scen = ixmp.TimeSeries(test_mp, *args_all)
-    obs = scen.timeseries()
+    ts = ixmp.TimeSeries(test_mp, *args_all)
+    obs = ts.timeseries()
     exp = pd.DataFrame.from_dict({'region': ['World'] * 2,
                                   'variable': ['Testing'] * 2,
                                   'unit': ['???', '???'],
                                   'year': [2010, 2020],
                                   'value': [23.7, 23.8]})
-    npt.assert_array_equal(exp[cols_str], obs[cols_str])
-    npt.assert_array_almost_equal(exp['value'], obs['value'])
+    assert_frame_equal(exp[cols_str], obs[cols_str])
+    assert_series_equal(exp['value'], obs['value'])
 
-    scen.check_out(timeseries_only=True)
+    ts.check_out(timeseries_only=True)
     df = pd.DataFrame.from_dict({'region': ['Hell'],
                                  'variable': ['Testing'],
                                  'unit': ['???'],
@@ -182,9 +193,9 @@ def test_timeseries_edit_with_region_synonyms(test_mp):
                                  '2030': [24.7],
                                  '2040': [25.7],
                                  '2050': [25.8]})
-    scen.preload_timeseries()
-    scen.add_timeseries(df)
-    scen.commit('updating timeseries in IAMC format')
+    ts.preload_timeseries()
+    ts.add_timeseries(df)
+    ts.commit('updating timeseries in IAMC format')
 
     exp = pd.DataFrame.from_dict(
         {'region': ['World'] * 6,
@@ -192,47 +203,47 @@ def test_timeseries_edit_with_region_synonyms(test_mp):
          'unit': ['???'] * 6,
          'year': [2000, 2010, 2020, 2030, 2040, 2050],
          'value': [21.7, 22.7, 23.7, 24.7, 25.7, 25.8]})
-    obs = scen.timeseries()
-    npt.assert_array_equal(exp[cols_str], obs[cols_str])
-    npt.assert_array_almost_equal(exp['value'], obs['value'])
+    obs = ts.timeseries()
+    assert_frame_equal(exp[cols_str], obs[cols_str])
+    assert_series_equal(exp['value'], obs['value'])
     test_mp.close_db()
 
 
 def test_timeseries_remove_single_entry(test_mp):
     args_single = ('Douglas Adams', 'test_remove_single')
 
-    scen = ixmp.Scenario(test_mp, *args_single, version='new', annotation='fo')
-    scen.add_timeseries(TS_DF.pivot_table(values='value', index=cols_str))
-    scen.commit('importing a testing timeseries')
+    ts = ixmp.TimeSeries(test_mp, *args_single, version='new', annotation='fo')
+    ts.add_timeseries(TS_DF.pivot_table(values='value', index=cols_str))
+    ts.commit('importing a testing timeseries')
 
-    scen = ixmp.Scenario(test_mp, *args_single)
-    assert_timeseries(scen, TS_DF)
+    ts = ixmp.TimeSeries(test_mp, *args_single)
+    assert_timeseries(ts, TS_DF)
 
-    scen.check_out()
-    scen.remove_timeseries(TS_DF[TS_DF.year == 2010])
-    scen.commit('testing for removing a single timeseries data point')
+    ts.check_out()
+    ts.remove_timeseries(TS_DF[TS_DF.year == 2010])
+    ts.commit('testing for removing a single timeseries data point')
 
-    exp = TS_DF[TS_DF.year == 2020]
-    assert_timeseries(scen, exp)
+    exp = TS_DF[TS_DF.year == 2020].reset_index()
+    assert_timeseries(ts, exp)
 
 
 def test_timeseries_remove_all_data(test_mp):
     args_all = ('Douglas Adams', 'test_remove_all')
 
-    scen = ixmp.Scenario(test_mp, *args_all, version='new', annotation='fo')
-    scen.add_timeseries(TS_DF.pivot_table(values='value', index=cols_str))
-    scen.commit('importing a testing timeseries')
+    ts = ixmp.TimeSeries(test_mp, *args_all, version='new', annotation='fo')
+    ts.add_timeseries(TS_DF.pivot_table(values='value', index=cols_str))
+    ts.commit('importing a testing timeseries')
 
-    scen = ixmp.Scenario(test_mp, *args_all)
-    assert_timeseries(scen, TS_DF)
+    ts = ixmp.TimeSeries(test_mp, *args_all)
+    assert_timeseries(ts, TS_DF)
 
     exp = TS_DF.copy()
     exp['variable'] = 'Testing2'
 
-    scen.check_out()
-    scen.add_timeseries(exp)
-    scen.remove_timeseries(TS_DF)
-    scen.commit('testing for removing a full timeseries row')
+    ts.check_out()
+    ts.add_timeseries(exp)
+    ts.remove_timeseries(TS_DF)
+    ts.commit('testing for removing a full timeseries row')
 
-    assert scen.timeseries(region='World', variable='Testing').empty
-    assert_timeseries(scen, exp)
+    assert ts.timeseries(region='World', variable='Testing').empty
+    assert_timeseries(ts, exp)

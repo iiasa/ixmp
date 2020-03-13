2f921907b6d59b05bb1070db96ea39e04f12a76f
Daniel Huppmann <dh@dergelbesalon.at>
2020-03-13 08:52:06 +0100

    add warning if importing timeseries with unexpected columns

--- ixmp/core.py
+++ ixmp/core.py
@@ -513,8 +513,17 @@ class TimeSeries:
             # Wide format: set index columns
             df.set_index(index_cols, inplace=True)
 
-        # Discard non-numeric columns, e.g. 'model', 'scenario'
+        # Discard non-numeric columns, e.g. 'model', 'scenario',
+        # write warning about non-expected cols to log
+        # TODO: please use `utils.numcols` here to avoid duplication of code
+        # TODO: and one remark: this should work on the column name,
+        # TODO: not the datatype of the column values
         num_cols = [pd.api.types.is_numeric_dtype(dt) for dt in df.dtypes]
+        other_cols = [i if i not in ['model', 'scenario'] + num_cols
+                      for i in df.columns]
+        if not other_cols.empty:
+            logger().warning(f'dropping index columns {other_cols} from data')
+
         df = df.iloc[:, num_cols]
 
         # Columns (year) as integer

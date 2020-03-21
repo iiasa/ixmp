5b8a70504eeb29d463bdb9e7b4cd4e60bd920715
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-03-21 11:16:36 +0100

    Handle old- and new-style sheets for index sets

--- ixmp/backend/io.py
+++ ixmp/backend/io.py
@@ -8,37 +8,39 @@ log = logging.getLogger(__name__)
 
 def s_write_excel(be, s, path):
     """Write *s* to a Microsoft Excel file at *path*."""
-    # List items to be exported
+    # item name -> ixmp type
     name_type = {}
     for ix_type in ('set', 'par', 'var', 'equ'):
-        for name in be.list_items(s, ix_type):
-            name_type[name] = ix_type
+        name_type.update({n: ix_type for n in be.list_items(s, ix_type)})
 
     # Open file
     writer = pd.ExcelWriter(path, engine='xlsxwriter')
 
-    # Extract and write data
-    for name, ix_type in name_type.items():
-        data = be.item_get_elements(s, ix_type, name)
-
-        if isinstance(data, dict):
-            data = pd.Series(data, name=name).to_frame()
-            if ix_type in ('par', 'var', 'equ'):
-                # Scalar parameter
-                data = data.transpose()
-        elif isinstance(data, pd.Series):
-            data.name = name
-
-        if data.empty:
-            continue
-
-        data.to_excel(writer, sheet_name=name, index=False)
-
-    # Also write the name -> type map
+    # Write the name -> type map
     pd.Series(name_type, name='ix_type') \
       .rename_axis(index='item') \
       .reset_index() \
       .to_excel(writer, sheet_name='ix_type_mapping', index=False)
+
+    for name, ix_type in name_type.items():
+        # Extract data: dict, pd.Series, or pd.DataFrame
+        data = be.item_get_elements(s, ix_type, name)
+
+        if isinstance(data, dict):
+            # Scalar equ/par/var: series with index like 'value', 'unit'.
+            # Convert to DataFrame with 1 row.
+            data = pd.Series(data, name=name) \
+                     .to_frame() \
+                     .transpose()
+        elif isinstance(data, pd.Series):
+            # Index set: use own name as the header
+            data.name = name
+
+        # Write empty sets, but not equ/par/var
+        if ix_type != 'set' and data.empty:
+            continue
+
+        data.to_excel(writer, sheet_name=name, index=False)
 
     writer.save()
 
@@ -62,67 +64,103 @@ def s_read_excel(be, s, path, add_units=False, init_items=False,
     """
     log.info(f'Reading data from {path}')
 
-    # Get item name -> ixmp type mapping
+    # Get item name -> ixmp type mapping as a pd.Series
     xf = pd.ExcelFile(path)
-    name_type = xf.parse('ix_type_mapping').set_index('item')['ix_type']
+    name_type = xf.parse('ix_type_mapping', index_col='item')['ix_type']
 
-    # List of sets to add
+    # List of *set name, data) to add
     sets_to_add = [(n, None) for n in name_type.index[name_type == 'set']]
 
-    # Add sets
+    # Add sets in two passes:
+    # 1. Index sets, required to initialize other sets.
+    # 2. Sets indexed by others.
     for name, data in sets_to_add:
         first_pass = data is None
         if first_pass:
+            # Read data
             data = xf.parse(name)
-        if (first_pass and data.columns == [name]) or not first_pass:
+
+        if (first_pass and len(data.columns) == 1) or not first_pass:
             # Index set or second appearance; add immediately
+            idx_sets = data.columns
+
             if init_items:
-                idx_sets = data.columns if len(data.columns) > 1 else None
+                # Determine index set(s) for this set
+                if len(idx_sets) == 1:
+                    if idx_sets == [0]:
+                        # Old-style export with uninformative '0' as a column
+                        # header; assume it's an index set
+                        log.warning(f"Add {name} with header '0' as index set")
+                        idx_sets = None
+                    elif idx_sets == [name]:
+                        # Set's own name as column header -> an index set
+                        idx_sets = None
+                    else:
+                        pass  # 1-D set indexed by another set
+
                 s.init_set(name, idx_sets)
-            if data.columns == [name]:
+
+            if idx_sets is None or len(idx_sets) == 1:
+                # Convert DataFrame into a 1-D vector
                 data = data[name].values
+
             s.add_set(name, data)
         else:
             # Reappend to the list to process later
-            sets_to_add.append[(name, data)]
+            sets_to_add.append((name, data))
 
     if commit_steps:
-        s.commit(f'Loaded initial data from {path}')
+        s.commit(f'Loaded sets from {path}')
         s.check_out()
 
     if add_units:
+        # List of existing units for reference
         units = set(be.get_units())
 
     # Read parameter data
     for name, ix_type in name_type[name_type != 'set'].items():
         if ix_type in ('equ', 'var'):
-            log.info(f'Not importing item {name!r} of type {ix_type!r}')
+            log.info(f'Cannot import {ix_type} {name!r}')
             continue
+
+        # Only parameters beyond this point
 
         df = xf.parse(name)
 
         if add_units:
+            # New units appearing in this parameter
             to_add = set(df['unit'].unique()) - units
+
             for unit in to_add:
-                log.info(f'Adding missing unit: {unit}')
+                log.info(f'Add missing unit: {unit}')
                 be.set_unit(unit, f'Loaded from {path}')
 
-            # Update the set
+            # Update the reference set to avoid re-adding these units
             units |= to_add
 
+        # NB if equ/var were imported, also need to filter 'lvl', 'mrg' here
         idx_sets = list(
             filter(lambda v: v not in ('value', 'unit'), df.columns)
         )
 
         if init_items:
+            # Same as init_scalar if idx_sets == []
             s.init_par(name, idx_sets)
 
         if not len(idx_sets):
-            # No index sets -> scalar parameter
+            # No index sets -> scalar parameter; must supply empty 'key' column
+            # for add_par()
             df['key'] = None
 
         s.add_par(name, df)
 
         if commit_steps:
-            s.commit(f'Loaded {name} from {path}')
+            # Commit after every parameter
+            s.commit(f'Loaded {ix_type} {name!r} from {path}')
             s.check_out()
+
+    # if commit_steps:
+    #     # No changes since check_out() above
+    #     s.discard_changes()
+    # else:
+    #     s.commit(f'Loaded data from {path}')

7ef7e6bc92d13363d772010531df75dec5a72fae
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-04-26 21:13:19 +0200

    Adjust test_computationerror_ipython for Python 3.6

--- ixmp/tests/reporting/test_exceptions.py
+++ ixmp/tests/reporting/test_exceptions.py
@@ -3,6 +3,9 @@ import re
 from ixmp.testing import get_cell_output, run_notebook
 
 
+# The TypeError message differs:
+# - Python 3.6: "must be str, not float"
+# - Python 3.7: "can only concatenate str (not "float") to str"
 EXPECTED = re.compile(r"""computing 'test' using:
 
 \(<function fail at \w+>,\)
@@ -12,7 +15,7 @@ Use Reporter.describe\(...\) to trace the computation.
 Computation traceback:
   File "<ipython-input-\d*-\w+>", line 4, in fail
     'x' \+ 3.4  # Raises TypeError
-TypeError: can only concatenate str \(not "float"\) to str
+TypeError: .*str.*float.*
 """)
 
 
@@ -20,4 +23,5 @@ def test_computationerror_ipython(test_data_path, tmp_path, tmp_env):
     fname = test_data_path / 'reporting-exceptions.ipynb'
     nb, _ = run_notebook(fname, tmp_path, tmp_env, allow_errors=True)
 
-    assert EXPECTED.match(get_cell_output(nb, 0, kind='evalue'))
+    observed = get_cell_output(nb, 0, kind='evalue')
+    assert EXPECTED.match(observed), observed

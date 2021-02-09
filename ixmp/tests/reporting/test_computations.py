7aa38a351f5e1001e74dd41f758bb5400630fb50
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-02-09 16:43:44 +0100

    Upstream test of reporting…update_scenario()

--- ixmp/tests/reporting/test_computations.py
+++ ixmp/tests/reporting/test_computations.py
@@ -1,9 +1,14 @@
+import logging
+from functools import partial
+
 from genno import Quantity
 from genno.testing import assert_qty_equal
 import pandas as pd
 import pytest
 
-from ixmp.reporting.computations import map_as_qty
+from ixmp import Scenario, Reporter
+from ixmp.reporting.computations import map_as_qty, update_scenario
+from ixmp.testing import assert_logs
 
 pytestmark = pytest.mark.usefixtures("parametrize_quantity_class")
 
@@ -32,3 +37,66 @@ def test_map_as_qty():
     )
 
     assert_qty_equal(exp, result)
+
+
+@pytest.mark.skip(reason="TODO update to use only ixmp features")
+def test_update_scenario(request, caplog, test_mp):
+    scen = Scenario(test_mp, model="update_scenario", scenario="update_scenario")
+
+    # Number of rows in the 'demand' parameter
+    N_before = len(scen.par("demand"))
+
+    # A Reporter used as calculation engine
+    calc = Reporter()
+
+    # Target Scenario for updating data
+    calc.add("target", scen)
+
+    # Create a pd.DataFrame suitable for Scenario.add_par()
+    units = "GWa"
+    demand = make_df(
+        "demand",
+        node="World",
+        commodity="electr",
+        level="secondary",
+        year=ScenarioInfo(scen).Y[:10],
+        time="year",
+        value=1.0,
+        unit=units,
+    )
+
+    # Add to the Reporter
+    calc.add("input", demand)
+
+    # Task to update the scenario with the data
+    calc.add("test 1", (partial(update_scenario, params=["demand"]), "target", "input"))
+
+    # Trigger the computation that results in data being added
+    with assert_logs(caplog, "'demand' ← 10 rows", at_level=logging.DEBUG):
+        # Returns nothing
+        assert calc.get("test 1") is None
+
+    # Rows were added to the parameter
+    assert len(scen.par("demand")) == N_before + len(demand)
+
+    # Modify the data
+    demand["value"] = 2.0
+    demand = demand.iloc[:5]
+    # Convert to a Quantity object
+    input = Quantity(
+        demand.set_index("node commodity level year time".split())["value"],
+        name="demand",
+        units=units,
+    )
+    # Re-add
+    calc.add("input", input)
+
+    # Revise the task; the parameter name ('demand')
+    calc.add("test 2", (update_scenario, "target", "input"))
+
+    # Trigger the computation
+    with assert_logs(caplog, "'demand' ← 5 rows"):
+        calc.get("test 2")
+
+    # Only half the rows have been updated
+    assert scen.par("demand")["value"].mean() == 1.5

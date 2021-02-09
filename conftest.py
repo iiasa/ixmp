603ef614f0993b74fcfd0910cf15de0192a75048
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-02-09 20:09:28 +0100

    Lint conftest.py

--- conftest.py
+++ conftest.py
@@ -1,4 +1,4 @@
-pytest_plugins = ["ixmp.testing"]
+pytest_plugins = ("ixmp.testing",)
 
 
 # Hooks
@@ -10,8 +10,8 @@ def pytest_addoption(parser):
         action="store",
         default=-1,
         help=(
-            "Memory limit, in MiB, for the Java Virtual Machine (JVM) "
-            "started by the ixmp JDBCBackend"
+            "Memory limit, in MiB, for the Java Virtual Machine (JVM) started by the "
+            "ixmp JDBCBackend"
         ),
     )
     parser.addoption(
@@ -19,7 +19,7 @@ def pytest_addoption(parser):
         action="store",
         default="DATA:-1",
         help=(
-            "Limit a Python resource via the ixmp.testing.resource_limit "
-            "fixture. Use e.g. 'DATA:500' to limit RLIMIT_DATA to 500 MiB"
+            "Limit a Python resource via the ixmp.testing.resource_limit fixture. Use "
+            "e.g. 'DATA:500' to limit RLIMIT_DATA to 500 MiB"
         ),
     )

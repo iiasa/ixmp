22e250106848d76c6230316859fc78216efd5fdc
Laura Wienpahl <57132039+LauWien@users.noreply.github.com>
2022-05-04 08:20:08 +0200

    Use combined logo

--- doc/conf.py
+++ doc/conf.py
@@ -80,7 +80,7 @@ html_css_files = ["custom.css"]
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "_static/logo_white.png"
+html_logo = "_static/combined-logo-white.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

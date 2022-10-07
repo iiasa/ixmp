69547b4f399e0bcc6dac953292f483038e4a1917
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2022-10-07 18:34:07 +0200

    Improve remote_head identification, logging on RTD, GHA

--- ixmp/utils/sphinx_linkcode_github.py
+++ ixmp/utils/sphinx_linkcode_github.py
@@ -5,7 +5,7 @@ Expanded from https://github.com/sphinx-doc/sphinx/issues/1556.
 
 import inspect
 from pathlib import Path
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
 from sphinx.util import logging
 
@@ -15,9 +15,13 @@ if TYPE_CHECKING:
 log = logging.getLogger(__name__)
 
 
-def _git_remote_head(app: "sphinx.application.Sphinx") -> str:
+def find_remote_head_git(app: "sphinx.application.Sphinx") -> Optional[str]:
     """Use git to identify the name of the remote branch containing the code."""
-    import git
+    try:
+        import git
+    except ImportError:
+        log.info("GitPython not installed; cannot determine remote head")
+        return None
 
     repo = git.Repo(app.srcdir, search_parent_directories=True)
 
@@ -32,49 +36,50 @@ def _git_remote_head(app: "sphinx.application.Sphinx") -> str:
         if not refs:
             log.info(f"No remote branch for commit {commit}")
             raise ValueError
-    except ValueError:
+    except ValueError:  # Either no remote "origin", or raised explicitly
         # Same, but locally
         refs = list(filter(lambda b: b.commit == commit, repo.branches))  # type: ignore
         if not refs:
-            raise ValueError(f"Unable to identify a branch for commit {commit}")
+            log.info(f"Unable to identify a branch for commit {commit}")
+            return None
 
     # Use the first result, arbitrarily. If the commit hash matches, so will the code.
     return refs[0].name
 
 
-def remote_head(app: "sphinx.application.Sphinx") -> str:
+def find_remote_head(app: "sphinx.application.Sphinx") -> str:
     """Return a name for the remote branch containing the code."""
-    result = None
-    try:
-        # Use GitPython to retrieve the repo information
-        result = _git_remote_head(app)
-    except (ImportError, RuntimeError) as e:
-        log.info(e)
+    # Value from configuration
+    cfg_remote_head = app.config["linkcode_github_remote_head"]
+    # Use GitPython to retrieve the repo information
+    git_remote_head = find_remote_head_git(app)
 
-    # Use value from configuration
-    from_cfg = app.config["linkcode_github_remote_head"]
-    if from_cfg and remote_head:
-        log.info(
-            f"Configuration setting linkcode_github_remote_head={from_cfg}"
-            f"overrides value from local git: {result}"
-        )
-        result = from_cfg
-
-    assert result is not None
-
-    return result
+    if cfg_remote_head:
+        if git_remote_head:
+            log.info(
+                f"Configuration setting linkcode_github_remote_head={cfg_remote_head} "
+                f"overrides value from local git: {git_remote_head}"
+            )
+        return cfg_remote_head
+    elif git_remote_head:
+        return git_remote_head
+    else:
+        raise RuntimeError("Cannot determine a remote head")
 
 
 class GitHubLinker:
     def __init__(self):
         self.line_numbers = dict()
+        self._ln_printed = False
 
     def config_inited(self, app: "sphinx.application.Sphinx", config):
         self.file_root = Path(app.srcdir).parent
+        log.info(f"linkcode file root: {self.file_root}")
         self.base_url = (
             f"https://github.com/{config['linkcode_github_repo_slug']}/blob/"
-            + remote_head(app)
+            + find_remote_head(app)
         )
+        log.info(f"linkcode base URL: {self.base_url}")
 
     def autodoc_process_docstring(
         self, app: "sphinx.application.Sphinx", what, name: str, obj, options, lines
@@ -98,7 +103,8 @@ class GitHubLinker:
                 source_lines, start_line = inspect.getsourcelines(obj)
                 end_line = start_line + len(source_lines)
                 self.line_numbers[name] = (file, start_line, end_line)
-        except Exception:
+        except Exception as e:
+            log.info(e)
             pass
 
         # In case `__init__` is not documented, we call this manually to have it
@@ -120,10 +126,16 @@ class GitHubLinker:
         ):
             line_info = self.line_numbers.get(candidate)
             if line_info:
-                log.debug(f"Cannot find a code link for {combined!r}")
                 break
 
         if not line_info:
+            log.info(f"Cannot find a code link for {combined!r}")
+
+            # Display debug information
+            if not self._ln_printed:
+                log.info("Among:\n\n" + "\n".join(self.line_numbers.keys()))
+                self._ln_printed = True
+
             return
 
         file, start_line, end_line = line_info

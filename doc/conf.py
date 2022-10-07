2975c62659ca552de5b0dd651e1b3c5bbc70a27f
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2022-10-07 10:18:32 +0200

    Address warnings from Sphinx 5.2.1; reflow comments

--- doc/conf.py
+++ doc/conf.py
@@ -46,13 +46,13 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
+# List of patterns, relative to source directory, that match files and directories to
+# ignore when looking for source files. This pattern also affects html_static_path and
+# html_extra_path.
 exclude_patterns = ["_build", "README.rst", "Thumbs.db", ".DS_Store"]
 
-# A string of reStructuredText that will be included at the beginning of every
-# source file that is read.
+# A string of reStructuredText that will be included at the beginning of every source
+# file that is read.
 version = ixmp.__version__
 rst_prolog = r"""
 .. |MESSAGEix| replace:: MESSAGE\ :emphasis:`ix`
@@ -76,24 +76,23 @@ html_css_files = ["custom.css"]
 
 html_favicon = "_static/favicon.svg"
 
-# The name of an image file (relative to this directory) to place at the top
-# of the sidebar.
+# The name of an image file (relative to this directory) to place at the top of the
+# sidebar.
 html_logo = "_static/combined-logo-white.png"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+# Add any paths that contain custom static files (such as style sheets) here, relative
+# to this directory. They are copied after the builtin static files, so a file named
+# "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
+# The theme to use for HTML and HTML Help pages.
 html_theme = "sphinx_rtd_theme"
 
 # -- Options for sphinx.ext.extlinks ---------------------------------------------------
 
 extlinks = {
-    "issue": ("https://github.com/iiasa/ixmp/issue/%s", "#"),
-    "pull": ("https://github.com/iiasa/ixmp/pull/%s", "PR #"),
+    "issue": ("https://github.com/iiasa/ixmp/issue/%s", "#%s"),
+    "pull": ("https://github.com/iiasa/ixmp/pull/%s", "PR #%s"),
 }
 
 # -- Options for sphinx.ext.intersphinx ------------------------------------------------

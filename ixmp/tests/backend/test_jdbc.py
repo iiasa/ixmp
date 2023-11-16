4ad872618b9f04988da39dfe257d19df258c193d
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-11-16 14:16:41 +0100

    Test JDBCBackend.init_item()

--- ixmp/tests/backend/test_jdbc.py
+++ ixmp/tests/backend/test_jdbc.py
@@ -141,6 +141,36 @@ class TestJDBCBackend:
 
         assert dict(driver="hsqldb", path=tmp_path) == klass.handle_config(args, kwargs)
 
+    @pytest.mark.parametrize(
+        "name, idx_sets",
+        (
+            ("C", ["node", "year"]),
+            ("COST_NODAL", ["node", "year"]),
+            ("COST_NODAL_NET", ["node", "year"]),
+            ("DEMAND", ["node", "commodity", "level", "year", "time"]),
+            ("GDP", ["node", "year"]),
+            ("I", ["node", "year"]),
+            # Expected exception class raised on invalid arguments
+            pytest.param(
+                "C",
+                ["node", "year", "technology"],
+                marks=pytest.mark.xfail(raises=NotImplementedError),
+            ),
+        ),
+    )
+    def test_init_item(self, mp, be, name, idx_sets):
+        """init_item() supports items that have fixed indices in ixmp_source."""
+
+        # Create an ixmp.Scenario, coerce its scheme to MESSAGE, and then call
+        # Backend.init(). This creates a Java MsgScenario object by circumventing
+        # safeguards in Scenario.__init__().
+        s = ixmp.Scenario(mp, "model name", "scenario name", version="new")
+        s.scheme = "MESSAGE"
+        be.init(s, "")
+
+        # Initialize the item; succeeds
+        be.init_item(s, type="var", name=name, idx_sets=idx_sets, idx_names=idx_sets)
+
     def test_set_data_inf(self, mp):
         """:meth:`JDBCBackend.set_data` errors on :data:`numpy.inf` values."""
         # Make `mp` think it is connected to an Oracle database

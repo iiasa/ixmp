28be2b184e31422b46e259be98d41e8cf8e06205
Fridolin Glatter <glatter@iiasa.ac.at>
2024-03-18 10:02:16 +0100

    Increase default cell runtime on GHA

--- ixmp/tests/test_tutorials.py
+++ ixmp/tests/test_tutorials.py
@@ -9,11 +9,23 @@ from ixmp.testing import get_cell_output, run_notebook
 
 group_base_name = platform.system() + platform.python_version()
 
+GHA = "GITHUB_ACTIONS" in os.environ
+
+
+def default_args():
+    """Default arguments for :func:`.run_notebook."""
+    if GHA:
+        # Use a longer timeout
+        return dict(timeout=30)
+    else:
+        return dict()
+
 
 @pytest.mark.xdist_group(name=f"{group_base_name}-0")
 def test_py_transport(tutorial_path, tmp_path, tmp_env):
     fname = tutorial_path / "transport" / "py_transport.ipynb"
-    nb, errors = run_notebook(fname, tmp_path, tmp_env)
+    args = default_args()
+    nb, errors = run_notebook(fname, tmp_path, tmp_env, **args)
     assert errors == []
 
     # FIXME use get_cell_by_name instead of assuming cell count/order is fixed
@@ -23,7 +35,8 @@ def test_py_transport(tutorial_path, tmp_path, tmp_env):
 @pytest.mark.xdist_group(name=f"{group_base_name}-0")
 def test_py_transport_scenario(tutorial_path, tmp_path, tmp_env):
     fname = tutorial_path / "transport" / "py_transport_scenario.ipynb"
-    nb, errors = run_notebook(fname, tmp_path, tmp_env)
+    args = default_args()
+    nb, errors = run_notebook(fname, tmp_path, tmp_env, **args)
     assert errors == []
 
     assert np.isclose(get_cell_output(nb, "scen-z")["lvl"], 153.675)
@@ -38,7 +51,8 @@ def test_py_transport_scenario(tutorial_path, tmp_path, tmp_env):
 )
 def test_R_transport(tutorial_path, tmp_path, tmp_env):
     fname = tutorial_path / "transport" / "R_transport.ipynb"
-    nb, errors = run_notebook(fname, tmp_path, tmp_env, kernel_name="IR")
+    args = default_args()
+    nb, errors = run_notebook(fname, tmp_path, tmp_env, kernel_name="IR", **args)
     assert errors == []
 
 
@@ -50,5 +64,6 @@ def test_R_transport(tutorial_path, tmp_path, tmp_env):
 )
 def test_R_transport_scenario(tutorial_path, tmp_path, tmp_env):
     fname = tutorial_path / "transport" / "R_transport_scenario.ipynb"
-    nb, errors = run_notebook(fname, tmp_path, tmp_env, kernel_name="IR")
+    args = default_args()
+    nb, errors = run_notebook(fname, tmp_path, tmp_env, kernel_name="IR", **args)
     assert errors == []

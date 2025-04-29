5f6a9d1ab393ada6ff713b933e618da982f6aa54
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2025-04-29 16:07:31 +0200

    Add .types.PlatformArgs

---
+++ ixmp/types.py
@@ -0,0 +1,13 @@
+"""Types for type hinting and checking of :mod:`ixmp` and downstream code."""
+
+from typing import NotRequired, TypedDict, Union
+
+
+class PlatformArgs(TypedDict, total=False):
+    """Arguments to :class:`.Platform`."""
+
+    name: NotRequired[Union[str, None]]
+    # NB The class itself has Literal["ixmp4", "jdbc"] first as an aid to completion in
+    #    IDE/interactive use, but for downstream code any str is valid, though may raise
+    #    in get_backend().
+    backend: NotRequired[Union[str, None]]
